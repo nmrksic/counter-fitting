aefa834f519f469a8b99b063cd6cfb88216a583b
Nikola Mrksic <nm480@cam.ac.uk>
2016-02-24 19:48:12 +0000

    Added fast VSP computation.

--- counterfitting.py
+++ counterfitting.py
@@ -10,7 +10,6 @@ from numpy.linalg import norm
 from numpy import dot
 from scipy.stats import spearmanr
 
-
 class ExperimentRun:
 	"""
 	This class stores all of the data and hyperparameters required for a counterfitting run. 
@@ -184,36 +183,6 @@ def extract_antonyms_from_dialogue_ontology(dialogue_ontology, vocabulary):
 	return antonym_list
 
 
-def compute_vsp_pairs(word_vectors, vocabulary, rho=0.2):
-	"""
-	This method returns a dictionary with all word pairs which are closer together than rho.
-	Each pair maps to the original distance in the vector space. 
-	"""
-	print "Pre-computing all the word pairs which are to be enforced via the VSP term."
-	print "Warning: use of the VSP term increases experiment time (~1h)."
-	
-	vocabulary = list(vocabulary)
-	vsp_pairs = {}
-	t_initial = time.time()
-
-	for idx, word_i in enumerate(vocabulary):
-		for j in range(idx + 1, len(vocabulary)):
-
-			word_j = vocabulary[j]
-			original_distance = distance(word_vectors[word_i], word_vectors[word_j])
-
-			if original_distance <= rho:
-				vsp_pairs[(word_i, word_j)] = original_distance 
-				vsp_pairs[(word_j, word_j)] = original_distance
-
-		if idx % 500 == 0: 
-			print "Progress:", round((float(idx) / len(vocabulary)) * 100, 3), "percent." \
-				  "# Pairs:", len(vsp_pairs), "Time:", round(time.time() - t_initial, 1), "seconds."
-	
-	print "There are", len(vsp_pairs), "VSP relations to enforce for rho = ", rho
-	return vsp_pairs
-
-
 def distance(v1, v2, normalised_vectors=True):
 	"""
 	Returns the cosine distance between two vectors. 
@@ -223,6 +192,83 @@ def distance(v1, v2, normalised_vectors=True):
 		return 1 - dot(v1, v2)
 	else:
 		return 1 - dot(v1, v2) / ( norm(v1) * norm(v2) )
+
+
+def compute_vsp_pairs(word_vectors, vocabulary, rho=0.2):
+	"""
+	This method returns a dictionary with all word pairs which are closer together than rho.
+	Each pair maps to the original distance in the vector space. 
+
+	In order to manage memory, this method computes dot-products of different subsets of word 
+	vectors and then reconstructs the indices of the word vectors that are deemed to be similar.
+	"""
+	print "Pre-computing all the word pairs which are to be enforced via the VSP term. Rho =", rho
+	
+	vsp_pairs = {}
+
+	threshold = 1 - rho 
+	vocabulary = list(vocabulary)
+	num_words = len(vocabulary)
+
+	step_size = 1000 # Number of word vectors to consider at each iteration. 
+	vector_size = random.choice(word_vectors.values()).shape[0]
+
+	# ranges of word vector indices to consider:
+	list_of_ranges = []
+
+	left_range_limit = 0
+	while left_range_limit < num_words:
+		curr_range = (left_range_limit, min(num_words, left_range_limit + step_size))
+		list_of_ranges.append(curr_range)
+		left_range_limit += step_size
+
+	range_count = len(list_of_ranges)
+
+	# now compute similarities between words in each word range:
+	for left_range in range(range_count):
+		for right_range in range(left_range, range_count):
+
+			# offsets of the current word ranges:
+			left_translation = list_of_ranges[left_range][0]
+			right_translation = list_of_ranges[right_range][0]
+
+			# copy the word vectors of the current word ranges:
+			vectors_left = numpy.zeros((step_size, vector_size), dtype="float32")
+			vectors_right = numpy.zeros((step_size, vector_size), dtype="float32")
+
+			# two iterations as the two ranges need not be same length (implicit zero-padding):
+			full_left_range = range(list_of_ranges[left_range][0], list_of_ranges[left_range][1])		
+			full_right_range = range(list_of_ranges[right_range][0], list_of_ranges[right_range][1])
+			
+			for iter_idx in full_left_range:
+				vectors_left[iter_idx - left_translation, :] = word_vectors[vocabulary[iter_idx]]
+
+			for iter_idx in full_right_range:
+				vectors_right[iter_idx - right_translation, :] = word_vectors[vocabulary[iter_idx]]
+
+			# now compute the correlations between the two sets of word vectors: 
+			dot_product = vectors_left.dot(vectors_right.T)
+
+			# find the indices of those word pairs whose dot product is above the threshold:
+			indices = numpy.where(dot_product >= threshold)
+
+			num_pairs = indices[0].shape[0]
+			left_indices = indices[0]
+			right_indices = indices[1]
+			
+			for iter_idx in range(0, num_pairs):
+				
+				left_word = vocabulary[left_translation + left_indices[iter_idx]]
+				right_word = vocabulary[right_translation + right_indices[iter_idx]]
+
+				if left_word != right_word:
+					# reconstruct the cosine distance and add word pair (both permutations):
+					score = 1 - dot_product[left_indices[iter_idx], right_indices[iter_idx]]
+					vsp_pairs[(left_word, right_word)] = score
+					vsp_pairs[(right_word, left_word)] = score
+		
+	print "There are", len(vsp_pairs), "VSP relations to enforce for rho =", rho, "\n"
+	return vsp_pairs
 
 
 def vector_partial_gradient(u, v, normalised_vectors=True):
@@ -254,6 +300,7 @@ def one_step_SGD(word_vectors, synonym_pairs, antonym_pairs, vsp_pairs, current_
 	oa_updates = {}
 	vsp_updates = {}
 
+	# AR term:
 	for (word_i, word_j) in antonym_pairs:
 
 		current_distance = distance(new_word_vectors[word_i], new_word_vectors[word_j])
@@ -270,6 +317,7 @@ def one_step_SGD(word_vectors, synonym_pairs, antonym_pairs, vsp_pairs, current_
 				gradient_updates[word_i] = gradient
 				update_count[word_i] = 1
 
+	# SA term:
 	for (word_i, word_j) in synonym_pairs:
 
 		current_distance = distance(new_word_vectors[word_i], new_word_vectors[word_j])
@@ -285,25 +333,24 @@ def one_step_SGD(word_vectors, synonym_pairs, antonym_pairs, vsp_pairs, current_
 			else:
 				gradient_updates[word_j] = -gradient
 				update_count[word_j] = 1
-				
-	if current_experiment.hyper_k3 > 0:
+	
+	# VSP term:			
+	for (word_i, word_j) in vsp_pairs:
 
-		for (word_i, word_j) in vsp_pairs:
+		original_distance = vsp_pairs[(word_i, word_j)]
+		new_distance = distance(new_word_vectors[word_i], new_word_vectors[word_j])
+		
+		if original_distance <= new_distance: 
 
-			original_distance = vsp_pairs[(word_i, word_j)]
-			new_distance = distance(new_word_vectors[word_i], new_word_vectors[word_j])
-			
-			if original_distance < new_distance and vsp_pairs[(word_i, word_j)] < 0.2: 
+			gradient = vector_partial_gradient(new_word_vectors[word_i], new_word_vectors[word_j]) 
+			gradient = gradient * current_experiment.hyper_k3 
 
-				gradient = vector_partial_gradient(new_word_vectors[word_i], new_word_vectors[word_j]) 
-				gradient = gradient * current_experiment.hyper_k3 
-
-				if word_i in gradient_updates:
-					gradient_updates[word_i] -= gradient
-					update_count[word_i] += 1
-				else:
-					gradient_updates[word_i] = -gradient
-					update_count[word_i] = 1
+			if word_i in gradient_updates:
+				gradient_updates[word_i] -= gradient
+				update_count[word_i] += 1
+			else:
+				gradient_updates[word_i] = -gradient
+				update_count[word_i] = 1
 
 	for word in gradient_updates:
 		# we've found that scaling the update term for each word helps with convergence speed. 
@@ -334,7 +381,7 @@ def counter_fit(current_experiment):
 		if antonym_pair in synonyms:
 			synonyms.remove(antonym_pair)
 		if antonym_pair in vsp_pairs:
-			vsp_pairs.remove(antonym_pair)
+			del vsp_pairs[antonym_pair]
 
 	max_iter = 20
 	print "Ready to run optimisation procedure for", max_iter, "SGD steps."
@@ -419,8 +466,6 @@ def run_experiment(config_filepath):
 	"""
 	current_experiment = ExperimentRun(config_filepath)
 	
-	#print_word_vectors(current_experiment.pretrained_word_vectors, "word_vectors/counter-fitted-vectors.txt")
-
 	print "\nSimLex score (Spearman's rho coefficient) of initial vectors is:", \
 		   simlex_analysis(current_experiment.pretrained_word_vectors), "\n"
 	
